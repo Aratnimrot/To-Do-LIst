1f36fcadfb2fa26d21ae7b24fc468cd623a540a0
Arati <ashishnimrot1290@gmail.com>
2024-11-27 22:42:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import TodoList from "./Components/TodoList";
+
+const App = () => {
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+  );
+};
+
+export default App;
